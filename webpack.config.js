6738bcef969a6f82ecd44b2d5a6c2005723cfb05
Renien Joseph <renien.john@gmail.com>
2016-06-12 21:53:55 +0530

    Updated with  module building for CSS, images and font

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   devtool: 'eval',
@@ -16,10 +17,56 @@ module.exports = {
     extensions: ['', '.js', '.jsx']
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
+    loaders: [
+      {
+          test: /\.jsx?$/,
+          loaders: ['babel'],
+          include: path.join(__dirname, 'src')
+      }, 
+      //TBD > Need to add the regex loader for images and font 
+      //http://stackoverflow.com/questions/31180570/webpack-can-not-load-font-file-unexpected-token
+      {       
+          test: /\.css$/,
+          exclude: /.*\.min.css/,
+          loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      }, 
+      {
+          test: /\.less$/,
+          loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')
+      }, {
+          test: /\.png$/,
+          loader: 'url-loader?prefix=img/&limit=5000'
+      }, {
+          test: /\.jpg$/,
+          loader: 'url-loader?prefix=img/&limit=5000'
+      }, {
+          test: /\.gif$/,
+          loader: 'url-loader?prefix=img/&limit=5000'
+      }, {
+          test: /\.woff$/,
+          loader: 'url-loader?prefix=font/&limit=5000'
+      },{
+          test: /\.woff2$/,
+          loader: 'url-loader?prefix=font/&limit=5000'
+      }, {
+          test: /\.eot$/,
+          loader: 'file-loader?prefix=font/'
+      }, {
+          test: /\.ttf$/,
+          loader: 'file-loader?prefix=font/'
+      }, {
+          test: /\.svg$/,
+          loader: 'file-loader?prefix=font/'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin("bundle.css")
+  ]
 };

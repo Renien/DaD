2e3b2301eb9c0f5e7491adf9d7c780a623d391a0
Renien Joseph <renien.john@gmail.com>
2016-06-14 05:52:53 +0530

    created the abstract layout

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
 import {observer} from 'mobx-react';
+import LayOutCSS  from '../styles/layout.css';
 
 @observer
 class MainLayout extends Component {
     render() {
         return (
-            <div>
-                Hello World!!!
+            <div className="ui container layout">
+                <div className="segment ui">
+                    Header
+                </div>
+                <div className="segment ui">
+                    Content
+                </div>
+                <div className="segment ui">
+                    Footer
+                </div>
             </div>
         );
     }

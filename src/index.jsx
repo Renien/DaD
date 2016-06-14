b9d4501c39189ca1a94715caeee695d4c7886033
Renien Joseph <renien.john@gmail.com>
2016-06-14 05:54:32 +0530

    reorder the imports to override sematic UI

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainLayout from './components/MainLayout';
-import appState from './stores/index.jsx';
 import SematicCSS from 'semantic-ui/dist/semantic.css';
 import Sematic from 'semantic-ui/dist/semantic.js';
+import MainLayout from './components/MainLayout';
+import appState from './stores/index.jsx';
+
 
 ReactDOM.render(<MainLayout appState={appState} />, document.getElementById('root'));

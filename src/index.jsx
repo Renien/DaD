794f41183b87fa25268960a837b172132dd80b2f
Renien <renien.john@gmail.com>
2016-06-19 14:28:07 +0530

    Incoperate the react-router

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import SematicCSS from 'semantic-ui/dist/semantic.css';
 import Sematic from 'semantic-ui/dist/semantic.js';
 import MainLayout from './components/MainLayout';
-import appState from './stores/index.jsx';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import BodyLayout from './components/BodyLayout';
+import SplashScreen from './components/SplashScreen';
 
-
-ReactDOM.render(<MainLayout appState={appState} />, document.getElementById('root'));
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <Route path="/" component={SplashScreen}/>
+        <Route path="/:algotype" component={MainLayout}/>
+    </Router>, document.getElementById('root'));
